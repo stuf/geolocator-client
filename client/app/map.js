cafcf76d5a375dca96d6c5c5c9f110d46c729fcc
stuf <stefan.rimaila@gmail.com>
2013-01-15 16:56:49 +0200

    Performing checks and basic maps behavior.

--- client/app/map.js
+++ client/app/map.js
@@ -1,8 +1,35 @@
 define("app/map", [], function () {
 	"use strict";
 
-	var Map = function () {
+	if (!("map" in window.geolocator)) {
+		window.geolocator.map = {};
 
+		var _geolocator = window.geolocator;
+	}
+
+	var Map = function (options) {
+		this.config = {
+			map: null,
+			mapHolder: "map",
+			mapOptions: {
+				zoom: 8
+			}
+		};
+
+		this.init();
+	};
+
+	Map.prototype = {
+		init: function () {
+			this.config.map =
+				new google.maps.Map(document.getElementById(this.config.mapHolder), this.config.mapOptions);
+
+
+		},
+
+		setLocation: function () {},
+
+		update: function () {}
 	};
 
 	return Map;
